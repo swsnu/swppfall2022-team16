8816211194f27f1804281c03e26ba106a3fa194b
연태영 <dusxodud98@snu.ac.kr>
2022-11-03 16:09:30 +0900

    Changed backend structure

--- backend/mybridgeUS/asgi.py
+++ backend/mybridgeUS/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bridgeUS project.
+ASGI config for mybridgeUS project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bridgeUS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mybridgeUS.settings')
 
 application = get_asgi_application()
