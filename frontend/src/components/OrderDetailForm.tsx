faabe877a9b2777a31d6f7790c9fb95eaba1765b
SungwonShim <ssw1998tt@snu.ac.kr>
2022-11-05 17:28:15 +0900

    orderform

--- frontend/src/components/OrderDetailForm.tsx
+++ frontend/src/components/OrderDetailForm.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
-/*eslint-disable */
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import { Stack } from 'react-bootstrap'
 
 export default function OrderDetailForm (): JSX.Element {
-  return <div>
-    Hi OrderForm!
-  </div>
+  return (
+      <Stack direction = 'vertical' gap = {3}>
+  <h3>Your Order</h3>
+  <OrderDetailForm/>
+  <Stack direction = 'horizontal' gap= {5}>
+    <Card style={{ width: '10rem' }}>
+      <Card.Body>
+        <Card.Title>Express Shipping</Card.Title>
+        <Card.Text>
+          1-3 Business days
+        </Card.Text>
+        <Card.Text>
+          $10.00
+        </Card.Text>
+        <Button variant="primary">Select</Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '10rem' }}>
+      <Card.Body>
+        <Card.Title>Standard Shipping</Card.Title>
+        <Card.Text>
+          4-7 Business days
+        </Card.Text>
+        <Card.Text>
+          $5.00
+        </Card.Text>
+        <Button variant="primary">Select</Button>
+      </Card.Body>
+    </Card>
+  </Stack>
+</Stack>
+  );
 }
