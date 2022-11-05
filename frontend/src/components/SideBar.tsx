255b0f5b800d36d5df52c6761e7d8cffa657a21c
taeminers <taeminers@snu.ac.kr>
2022-11-05 13:02:57 +0900

    sidebar done

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
 /*eslint-disable */
 
 export default function SideBar (): JSX.Element {
-  return <div>
-    Hi SideBar!
-  </div>
+  return (
+    <>
+    <Navbar sticky = "top" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="./">{' '}BridgeUs</Navbar.Brand>
+      </Container>
+    </Navbar>
+  </>
+  )
 }
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -3,10 +3,11 @@ import { Stack } from 'react-bootstrap'
 import LoginForm from '../components/LoginForm'
 import SideBar from '../components/SideBar'
 
-/*eslint-disable */
 
+/*eslint-disable */
+/*eslint no-multiple-empty-lines: "error"*/
 export default function LoginPage (): JSX.Element {
-  return (<Stack direction='horizontal'>
+  return (<Stack direction='vertical' gap ={3}>
     <SideBar />
     <LoginForm />
   </Stack>)
