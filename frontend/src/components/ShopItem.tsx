3142626a3549f904824a76aac1fe56d6c52c9a54
Young-in <daddy321@snu.ac.kr>
2022-11-05 07:58:08 +0000

    Add fetching reviews from backend

--- frontend/src/components/ShopItem.tsx
+++ frontend/src/components/ShopItem.tsx
@@ -4,17 +4,17 @@ import { useNavigate } from 'react-router-dom'
 import { ShopItemInfo } from '../store/slices/shopitem';
 /*eslint-disable */
 
-export default function ShopItem (props: { shopItem: ShopItemInfo | null }): JSX.Element {
+export default function ShopItem (props: { shopItem: ShopItemInfo }): JSX.Element {
   const shopItem = props.shopItem
 
   const navigate = useNavigate();
   
   return <div>
-    <Card onClick = {() => navigate(`/product/${shopItem?.id}`)} style={{ width: '18rem' }}>
+    <Card onClick = {() => navigate(`/product/${shopItem.id}`)} style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://img.sbs.co.kr/newsnet/etv/upload/2020/10/28/30000654805_1280.jpg" style={{ width: '18rem' }} />
       <Card.Body>
-        <Card.Title as= "h3">{shopItem?.name}</Card.Title>
-        <Card.Text as= "h5">{shopItem?.seller}</Card.Text>
+        <Card.Title as= "h3">{shopItem.name}</Card.Title>
+        <Card.Text as= "h5">{shopItem.seller}</Card.Text>
       </Card.Body>
     </Card>
   </div>
