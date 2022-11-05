d6e3431f1e01f7ac59262d661e919d3a3853e583
taeminers <taeminers@gmail.com>
2022-11-05 16:22:33 +0900

    filter final

--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -7,13 +7,15 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 /*eslint-disable */
 export interface theFilter {
-    key : number,
+    categoryName : string,
+    list : string[],
+
 }
 
-export default function Filter (props : theFilter): JSX.Element {
+export default function Filter (): JSX.Element {
   return (
     <>
-      <DropdownButton id="dropdown-basic-button" title={props.key}>
+      <DropdownButton id="dropdown-basic-button" title = "filter">
       <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
       <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
       <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
