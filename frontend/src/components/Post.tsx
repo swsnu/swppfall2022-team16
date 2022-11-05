882d7863f64ecc6ea26f6f3e70cf9ce7d7e788b3
SungwonShim <ssw1998tt@snu.ac.kr>
2022-11-05 13:21:58 +0900

    post and shop item

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
+import CardHeader from 'react-bootstrap/esm/CardHeader';
+import { Navigate, useNavigate } from 'react-router-dom'
 /*eslint-disable */
 
-export default function Post (): JSX.Element {
+export default function Post(): JSX.Element {
+
+  const navigate = useNavigate();
+  
   return <div>
-    Hi Post!
+    <Card onClick = {() => navigate("/community/:id")} style={{ width: '18rem' }}>
+      <Card.Header>Trending Post</Card.Header>
+      <Card.Img variant="top" src="https://img.sbs.co.kr/newsnet/etv/upload/2020/10/28/30000654805_1280.jpg" style={{ width: '18rem' }} />
+      <Card.Body>
+        <Card.Text as= "h5">@karina</Card.Text>
+      </Card.Body> 
+    </Card>
   </div>
 }
--- frontend/src/components/ShopItem.tsx
+++ frontend/src/components/ShopItem.tsx
@@ -11,8 +11,8 @@ export default function ShopItem (): JSX.Element {
     <Card onClick = {() => navigate("/product/:id")} style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://img.sbs.co.kr/newsnet/etv/upload/2020/10/28/30000654805_1280.jpg" style={{ width: '18rem' }} />
       <Card.Body>
-        <Card.Title as="h3">Item1</Card.Title>
-        <Card.Text as="h5">Seller1</Card.Text>
+        <Card.Title as= "h3">Item1</Card.Title>
+        <Card.Text as= "h5">Seller1</Card.Text>
       </Card.Body>
     </Card>
   </div>
