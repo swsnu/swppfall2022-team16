372b47228af6d5a0bd1d66c35e9009d911132744
taeminers <taeminers@gmail.com>
2022-11-05 13:59:15 +0900

    deleting prev push

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -8,41 +8,6 @@ import {useNavigate} from 'react-router-dom'
 export default function Banner (): JSX.Element {
   const navigate = useNavigate();
   return (
-    <div>
-    <Carousel>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="./bannerCarousel.png"
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <button onClick = {()=> navigate('./search/:id')} >SHOP NOW</button>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="./bannerCarousel.png"
-        alt="Second slide"
-      />
-
-      <Carousel.Caption>
-        <button onClick = {()=> navigate('./search/:id')} >SHOP NOW</button>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="./carousel2.png"
-        alt="Third slide"
-      />
-
-      <Carousel.Caption>
-        <button onClick = {()=> navigate('./search/:id')} >SHOP NOW</button>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
-  </div>
+    <div>Hi</div>
   )
 }
