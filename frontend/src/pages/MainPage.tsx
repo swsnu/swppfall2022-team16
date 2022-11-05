9a7bb24bbb747983c844eafb09dd3820a405e3c4
Young-in <daddy321@snu.ac.kr>
2022-11-05 06:58:33 +0000

    Add get for shopitems

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Banner from '../components/Banner'
 import Filter from '../components/Filter'
 import ShopItem from '../components/ShopItem'
 import TopBar from '../components/TopBar'
 import { AppDispatch } from '../store'
+import { fetchMainItems, selectShopItem } from '../store/slices/shopitem'
 /*eslint-disable */
 
 export default function MainPage (): JSX.Element {
   const dispatch = useDispatch<AppDispatch>()
+  const shopItemState = useSelector(selectShopItem)
+
+  useEffect(() => {
+    dispatch(fetchMainItems())
+  }, [dispatch])
+
   return (<div>
     <TopBar />
     <br/>
@@ -31,8 +38,8 @@ export default function MainPage (): JSX.Element {
       </Row>
       <Row>
         {
-          [0, 1, 2, 3].map((i) => <Col>
-            <ShopItem key={i} />
+          shopItemState.shopitems.map((shopItem) => <Col>
+            <ShopItem key={shopItem.id} shopItem={shopItem} />
           </Col>)
         }
       </Row>
