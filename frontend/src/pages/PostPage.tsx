46a004862eebc1cb1adb025ffdc2b0a9e78b3bdb
Young-in <daddy321@snu.ac.kr>
2022-11-05 05:54:49 +0000

    Add layout of PostPage

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import Post from '../components/Post'
+import PostComments from '../components/PostComments'
+import TopBar from '../components/TopBar'
 /*eslint-disable */
 
 export default function PostPage (): JSX.Element {
   return (<div>
-    <h1>PostPage</h1>
-    <p>Hi!</p>
+    <TopBar />
+    <Container>
+      <Row>
+        <Col>
+          <Post />
+        </Col>
+        <Col>
+          <PostComments />
+        </Col>
+      </Row>
+    </Container>
   </div>)
 }
