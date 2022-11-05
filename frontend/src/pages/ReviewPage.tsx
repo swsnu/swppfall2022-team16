1f1359e9ae20d83b0eddadaf44e5d24c7c3a444e
Young-in <daddy321@snu.ac.kr>
2022-11-05 05:34:17 +0000

    Add layout of ReviewPage and SearchPage

--- frontend/src/pages/ReviewPage.tsx
+++ frontend/src/pages/ReviewPage.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import ReviewForm from '../components/ReviewForm'
+import ShopItem from '../components/ShopItem'
+import TopBar from '../components/TopBar'
 
 /*eslint-disable */
 
 
 export default function ReviewPage (): JSX.Element {
   return (<div>
-    <h1>ReviewPage</h1>
-    <p>Hi!</p>
+    <TopBar />
+    <Container>
+      <Row>
+        <Col>
+          <h1>Write your review</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <ShopItem />
+        </Col>
+        <Col>
+          <ReviewForm />
+        </Col>
+      </Row>
+    </Container>
   </div>)
 }
