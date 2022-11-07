b831eaedc5eebfa6a478f58263c05236059a2fa2
Young-in <daddy321@snu.ac.kr>
2022-11-07 13:35:36 +0000

    Modify styles of MyPage

--- frontend/src/components/Purchased.tsx
+++ frontend/src/components/Purchased.tsx
@@ -12,16 +12,15 @@ export interface IProps {
 export default function Purchased (props : IProps): JSX.Element {
   return (
     <div className = "Purchases">
-      <h1>Purchases</h1>
       <Stack direction = "horizontal">
         <h5>{props.itemName}</h5>
         <h3>{"$" + props.itemPrice}</h3>
         <p>{props.shippingStatus}</p>
         <h3>{props.purchaseDate}</h3>
-        <Button variant = "secondary" type = "submit" onClick = {() => {}}>
-          Write your Review
-        </Button>
       </Stack>
+      <Button variant = "secondary" type = "submit" onClick = {() => {}}>
+        Write your Review
+      </Button>
     </div>
   );
 }
