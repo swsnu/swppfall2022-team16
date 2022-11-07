0685f1c1b8389e959762801d2cc63ae3fa2716d6
연태영 <dusxodud98@snu.ac.kr>
2022-11-07 23:15:21 +0900

    Added Comment Slice

--- frontend/src/store/slices/comment.ts
+++ frontend/src/store/slices/comment.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { RootState } from '..';
 
@@ -32,10 +32,50 @@ export const fetchComments = createAsyncThunk(
     }
 )
 
+export const postComment = createAsyncThunk(
+    "comment/postComment", async (
+    data : {
+        review_id: number;
+        content: string;
+    }, {dispatch}) => {
+    const response = await axios.post(`/api/review/${data.review_id}/comment/`, { content : data.content });
+    dispatch(commentActions.postComment(response.data));
+});
+
+export const putComment = createAsyncThunk(
+    "comment/putComment", async (commment : CommentInfo, {dispatch}) => {
+    await axios.put(`/api/comment/${commment.id}/`, commment);
+    dispatch(commentActions.putComment(commment));
+});
+
+export const deleteComment = createAsyncThunk(
+    "comment/deleteComment", async (id : number, {dispatch}) => {
+    await axios.delete(`/api/comment/${id}/`);
+    dispatch(commentActions.deleteComment( { targetId : id }));
+});
+
 export const commentSlice = createSlice({
     name: "comment",
     initialState,
-    reducers:{},
+    reducers:{
+        putComment: (state, action: PayloadAction<CommentInfo>) => {
+            state.comments =  state.comments.map(
+                (value) => {
+                    if (value.id === action.payload.id)
+                        return action.payload;
+                    else return value;    
+                }
+            );
+        },
+        postComment: (state, action: PayloadAction<CommentInfo>) => {
+            state.comments.push(action.payload);
+        },
+        deleteComment: (state, action: PayloadAction<{ targetId : Number }>) => {
+            state.comments =  state.comments.filter(
+                (value) => { return value.id !== action.payload.targetId }
+            );
+        },   
+    },
     extraReducers: (builder) => {
         builder.addCase(fetchComments.fulfilled, (state, action) => {
             const review_id = action.meta.arg
