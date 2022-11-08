2355fc2a5d69dda1ea1c9397c2099f74e8e3c633
연태영 <dusxodud98@snu.ac.kr>
2022-11-08 23:40:32 +0900

    added react persist

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,15 +5,22 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
+
+let persistor = persistStore(store);
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PersistGate loading={null} persistor={persistor}>  
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 )
 
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist';
 import userReducer from './slices/user'
 import userShopReducer from './slices/usershop'
 import shopitemReducer from './slices/shopitem'
@@ -6,17 +7,27 @@ import shopitemDetailReducer from './slices/shopitemdetail'
 import reviewReducer from './slices/review'
 import commentReducer from './slices/comment'
 import userorderReducer from './slices/userorder'
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({
+  user: userReducer,
+  usershop: userShopReducer,
+  shopitem: shopitemReducer,
+  shopitemdetail: shopitemDetailReducer,
+  review: reviewReducer,
+  comment: commentReducer,
+  userorder: userorderReducer
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    usershop: userShopReducer,
-    shopitem: shopitemReducer,
-    shopitemdetail: shopitemDetailReducer,
-    review: reviewReducer,
-    comment: commentReducer,
-    userorder: userorderReducer
-  }
+  reducer: persistedReducer,
 })
 
 export type RootState = ReturnType<typeof store.getState>
