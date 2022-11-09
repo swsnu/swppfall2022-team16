b9664a4f1064ea39a00aa2b26ecd214dd99d713d
Young-in <daddy321@snu.ac.kr>
2022-11-09 05:02:07 +0000

    Apply findAuthorName to Post and Review

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -8,6 +8,7 @@ import { AppDispatch } from '../store';
 import { fetchReviews, selectReview } from '../store/slices/review';
 import * as Icon from 'react-bootstrap-icons';
 import {AiFillLike} from "react-icons/ai"
+import { fetchUsers, selectUser, User } from '../store/slices/user';
 /*eslint-disable */
 
 export interface IProps {
@@ -18,13 +19,20 @@ export default function Post(props: IProps): JSX.Element {
   const dispatch = useDispatch<AppDispatch>()
   const navigate = useNavigate();
   const reviewState = useSelector(selectReview)
+  const userState = useSelector(selectUser)
   const [numLike, setNumLike] = useState(1000); // have to get info from DB
 
   useEffect(() => {
     dispatch(fetchReviews())
+    dispatch(fetchUsers)
   }, [dispatch])
   
   const likeButtonHandler = () => {setNumLike(numLike + 1)}; // axios command necessary
+  const review = reviewState.reviews.find((review) => review.id === props.id)
+  
+  const findAuthorName = (ID : number | undefined) => {
+    return userState.users.find((user : User) => {return (user.id === ID);})?.nickname;
+};
 
   return <div>
     <Card onClick = {() => navigate(`/community/${props.id}`)} style={{ width: '18rem' }}>
@@ -40,7 +48,7 @@ export default function Post(props: IProps): JSX.Element {
       </Card.ImgOverlay>
       <Card.Body>
         <Card.Text as= "h5">
-          {reviewState.reviews.find((review) => review.id === props.id)?.author}
+          {findAuthorName(review?.author)}
         </Card.Text>
       </Card.Body> 
     </Card>
--- frontend/src/components/Review.tsx
+++ frontend/src/components/Review.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Card, Stack } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import * as Icon from 'react-bootstrap-icons';
 import { ReviewInfo } from '../store/slices/review';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '../store';
+import { fetchUsers, selectUser, User } from '../store/slices/user';
 /*eslint-disable */
 
 export default function Review (props: { review: ReviewInfo }): JSX.Element {
+  const dispatch = useDispatch<AppDispatch>()
+  const userState = useSelector(selectUser)
   const review = props.review
+
+  useEffect(() => {
+    dispatch(fetchUsers)
+  }, [dispatch])
+
+  const findAuthorName = (ID : number | undefined) => {
+          return userState.users.find((user : User) => {return (user.id === ID);})?.nickname;
+      };
 
   const navigate = useNavigate();
   
@@ -15,7 +28,7 @@ export default function Review (props: { review: ReviewInfo }): JSX.Element {
       <Card.Img variant="top" src="https://img.sbs.co.kr/newsnet/etv/upload/2020/10/28/30000654805_1280.jpg" style={{ width: '18rem', height: '24rem', objectFit: 'cover'}} />
       <Card.Body>
         <Card.Title as= "h3">{review.content}</Card.Title>
-        <Card.Text as= "p">{review.author}</Card.Text>
+        <Card.Text as= "p">{findAuthorName(review.author)}</Card.Text>
         <Stack direction = 'horizontal'>
           <Icon.StarFill/>
           <Icon.StarFill/>
