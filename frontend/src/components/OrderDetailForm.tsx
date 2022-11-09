e2d3ba3a5ab01cda9a8e7e59e8bb037b3898ae3c
SungwonShim <ssw1998tt@snu.ac.kr>
2022-11-09 18:59:26 +0900

    temporary order detail form

--- frontend/src/components/OrderDetailForm.tsx
+++ frontend/src/components/OrderDetailForm.tsx
@@ -1,36 +1,50 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
-import { Stack } from 'react-bootstrap'
+import { Form, Stack } from 'react-bootstrap'
 
-export default function OrderDetailForm (): JSX.Element {
+export interface IProps {
+  itemName: string | undefined;
+  sellerName: string | undefined;
+  quantity: number;
+  price: number | undefined;
+  recommendedSize: string | undefined;
+}
+
+export default function OrderDetailForm (props : IProps): JSX.Element {
   return (
-      <Stack direction = 'vertical' gap = {3}>
-  <h3>Your Order</h3>
-  <OrderDetailForm/>
+  <Stack direction = 'vertical' gap = {3}>
+  <h3>{props.itemName}</h3>
+  <p>{props.sellerName}</p>
   <Stack direction = 'horizontal' gap= {5}>
     <Card style={{ width: '10rem' }}>
       <Card.Body>
-        <Card.Title>Express Shipping</Card.Title>
+        <Card.Title>★★★★☆</Card.Title>
         <Card.Text>
-          1-3 Business days
+          {"$" + props.price}
+        <Form>
+        <Form.Control type = "color" defaultValue="#563d7c" title="Choose your color"/>
+          <Stack direction = "horizontal" gap = {5}>
+          <Form.Select aria-label = "Quantity">
+            <option>Quantity</option>
+            <option value = "1">1</option>
+            <option value = "2">2</option>
+            <option value = "3">3</option>
+            <option value = "4">4</option>
+            <option value = "5">5</option>
+          </Form.Select>
+          <Form.Select aria-label = "Size">
+          <Form.Control placeholder = {"recommended size: " + props.recommendedSize}/>
+            <option>Size</option>
+            <option value = "1">S</option>
+            <option value = "2">M</option>
+            <option value = "3">L</option>
+          </Form.Select>
+          </Stack>
+        </Form>
         </Card.Text>
-        <Card.Text>
-          $10.00
-        </Card.Text>
-        <Button variant="primary">Select</Button>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '10rem' }}>
-      <Card.Body>
-        <Card.Title>Standard Shipping</Card.Title>
-        <Card.Text>
-          4-7 Business days
-        </Card.Text>
-        <Card.Text>
-          $5.00
-        </Card.Text>
-        <Button variant="primary">Select</Button>
+        <Button variant="primary">Add to Cart</Button>
+        <Button variant="secondary" /*onClick = {() => {useNavigate("/payment")}}*/>Buy Now</Button>
       </Card.Body>
     </Card>
   </Stack>
