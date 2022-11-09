eac76ebdd062f03e6f301118af1adbfdf24d276b
Young-in <daddy321@snu.ac.kr>
2022-11-09 15:39:27 +0000

    Improve MyPage layout

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -7,7 +7,7 @@ import CommunityAlert from '../components/CommunityAlert'
 import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch } from '../store'
 import Footer from '../components/Footer'
-import { Col, Container, ListGroup, Row } from 'react-bootstrap'
+import { Col, Container, Image, ListGroup, Row } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import { fetchOrders, selectUserOrder } from '../store/slices/userorder'
 /*eslint-disable */
@@ -32,13 +32,13 @@ export default function MyPage (): JSX.Element {
           <Col>
             <Card>
               <Card.Img variant="top" src="../mypagebackground.jpg" />
-                <Card.ImgOverlay>
-                  <img className = "profilepicture" src = '../mypageprofile.png' height = {150} width = {150} style={{ alignSelf: 'center' }}></img>
-                  <Card.Title>Alice</Card.Title>
-                </Card.ImgOverlay>
-                <Card.Body>
-                  <Button variant="primary">Edit</Button>
-                </Card.Body>
+              <Card.ImgOverlay style={{textAlign: 'center'}}>
+                <Image className = "profilepicture" src = '../mypageprofile.png' height = {150} width = {150} style={{ alignSelf: 'center', marginTop: '72px' }}></Image>
+                <Card.Title style={{fontSize: '30px', color: 'white'}}>Alice</Card.Title>
+              </Card.ImgOverlay>
+              <Card.Body style={{textAlign: 'center'}}>
+                <Button variant="primary">Edit</Button>
+              </Card.Body>
             </Card>
             <h1 className="Header-row  Header">Purchased</h1>
             <ListGroup>
