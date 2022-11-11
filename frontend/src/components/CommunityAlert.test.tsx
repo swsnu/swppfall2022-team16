e9a67a88d3c6b1a68ac5b5f3ca168f191799e087
SungwonShim <ssw1998tt@snu.ac.kr>
2022-11-11 18:52:23 +0900

    Community Alert test implemented

---
+++ frontend/src/components/CommunityAlert.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import CommunityAlert from "./CommunityAlert";
+
+const mockNavigate = jest.fn();
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useNavigate: () => mockNavigate,
+}));
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
+
+describe("<Community Alert />", () => {
+    it("should render without errors", () => {
+        render(<CommunityAlert newCommentAuthor = "team16" 
+        newCommentedPostId = {1}
+        newCommentPostedTime = {new Date()}
+        />);
+        const stackLink = screen.getByText("team16 commented on your post");
+        fireEvent.click(stackLink);
+        expect(mockNavigate).toHaveBeenCalled();
+        screen.getByText("0 hours ago");
+        })
+});
