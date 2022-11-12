0951480439c06ae27d2ab1cec5f62dd0c42be1d8
SungwonShim <ssw1998tt@snu.ac.kr>
2022-11-12 14:35:10 +0900

    Purchased test implemented

--- frontend/src/components/Purchased.test.tsx
+++ frontend/src/components/Purchased.test.tsx
@@ -1,23 +1,33 @@
 import { fireEvent, render, screen, waitFor} from "@testing-library/react";
 import Purchased from "./Purchased";
 import { renderWithProviders } from '../test-utils/mock';
+import { Route, Routes } from 'react-router-dom';
+
+const mockNavigate = jest.fn();
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useNavigate: () => mockNavigate,
+}));
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
+
 
 describe("<Purchased />", () => {
-    it("should render without errors", async () => {
-        renderWithProviders(<Purchased
-            itemName = "BridgeUs"
-            itemPrice = {100}
-            shippingStatus = ""
-            purchaseDate = ""
-            />);
-            screen.getByAltText("itemimage");
-            screen.getByText("BridgeUs");
-            screen.getByText("team16");
-            screen.getByText("Color");
-            screen.getByText("Size");
-            screen.getByText("Quantity");
-            screen.getByText("Black");
-            screen.getByText("L");
-            screen.getByText("1");
-        })
+    it("should render without errors", () => {
+        renderWithProviders(
+            <Purchased order = {{
+            id: 1,
+            user_id: 1,
+            item_id: 1,
+            status: "shipping"
+        }}/>);
+
+        screen.getByText("shipping");
+        const reviewButton = screen.getByText("Write your Review");
+        fireEvent.click(reviewButton);
+        expect(mockNavigate).toHaveBeenCalled();
+    })
 });
