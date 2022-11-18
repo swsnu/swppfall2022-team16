257fd11c2b11b93f94a857c8027a7ecfe3dce45c
SungwonShim <ssw1998tt@snu.ac.kr>
2022-11-18 14:35:34 +0900

    post detail through id

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -6,7 +6,7 @@ import { AppDispatch } from '../store'
 import { fetchReviews, selectReview } from '../store/slices/review'
 import { AiFillLike } from 'react-icons/ai'
 import { fetchUsers, selectUser, User } from '../store/slices/user'
-import { fetchMainItem, selectShopItem } from '../store/slices/shopitem'
+import { fetchMainItems, selectShopItem, ShopItemInfo } from '../store/slices/shopitem'
 /*eslint-disable */
 
 export interface IProps {
@@ -26,7 +26,7 @@ export default function Post(props: IProps): JSX.Element {
     dispatch(fetchReviews()).then(() => {
       setNumLike(reviewState.reviews.find((review) => review.id === props.id)?.likes ?? 0)
     })
-    dispatch(fetchMainItem(props.id))
+    dispatch(fetchMainItems())
     dispatch(fetchUsers())
   }, [dispatch])
   
@@ -37,11 +37,15 @@ export default function Post(props: IProps): JSX.Element {
     return userState.users.find((user : User) => {return (user.id === ID);})?.nickname;
 };
   console.debug(props.id)
-  console.debug(itemState.current_shopitem)
+  console.debug(itemState.shopitems)
+
+  const SpecificItem = (ID : number | undefined) => {
+    return itemState.shopitems.find((item : ShopItemInfo) => {return (item.id === ID);});
+};
 
   return <div>
-    <h1>name</h1>
-    <p>seller</p>
+    <h1>{SpecificItem(props.id)?.name}</h1>
+    <p>{SpecificItem(props.id)?.seller}</p>
     <Card onClick = {() => navigate(`/community/${props.id}`)} style={{ width: '18rem' }} border={hover ? 'primary' : ''} onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}>
       <Card.Img alt = "postimage" variant="top" src={review?.image_url} style={{ width: '17.9rem', height: '24rem', objectFit: 'cover'}} />
       <Card.ImgOverlay>
@@ -57,7 +61,7 @@ export default function Post(props: IProps): JSX.Element {
       <Card.Body>
         <Stack direction = 'horizontal'>
           <Card.Text as= "h5" data-testid = "test">
-            star
+            {SpecificItem(props.id)?.star}
           </Card.Text>
           <div className = "ms-auto">
           <Card.Text as= "h5" data-testid = "test">
