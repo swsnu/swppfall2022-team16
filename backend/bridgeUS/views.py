925a5cac0afc9af8b5d78d32bf4e1b413790affd
연태영 <dusxodud98@snu.ac.kr>
2022-11-19 14:34:28 +0900

    Update views.py

--- backend/bridgeUS/views.py
+++ backend/bridgeUS/views.py
@@ -511,7 +511,7 @@ def get_shopitemdetail_json(detail):
     return { 'id' : detail.id, 'mainitem' : detail.main_item.id, 'color' : detail.color, 'size': detail.size, 'left_amount' : detail.left_amount }    
 
 def get_shopitem_json(shopitem):
-    return { 'id': shopitem.id, 'name': shopitem.name, 'seller' : shopitem.seller.id, 'price' : shopitem.price, 'rating': shopitem.rating, 'type': shopitem.type, 'tag': shopitem.tag }
+    return { 'id': shopitem.id, 'image_url' : shopitem.image_url, 'name': shopitem.name, 'seller' : shopitem.seller.id, 'price' : shopitem.price, 'rating': shopitem.rating, 'type': shopitem.type, 'tag': shopitem.tag }
 
 def get_user_json(user):
     return { 'id' : user.id, 'username' : user.username, 'nickname' : user.nickname, 'gender' : user.gender, 'height' : user.height, 'weight': user.weight }     
