bb1d6fc0baeba4c15e83af0f72febb1b1f76e742
Young-in <daddy321@snu.ac.kr>
2022-11-26 03:30:14 +0000

    Update test on CommunityPage

--- frontend/src/pages/CommunityPage.test.tsx
+++ frontend/src/pages/CommunityPage.test.tsx
@@ -1,20 +1,21 @@
 import { MemoryRouter, Route, Routes } from "react-router-dom"
+import { ReviewState } from "../store/slices/review"
 import { ShopItemState } from "../store/slices/shopitem"
-import { renderWithProviders, stubShopItemState } from "../test-utils/mock"
+import { renderWithProviders, stubReviewState, stubShopItemState } from "../test-utils/mock"
 import CommunityPage from "./CommunityPage"
 
-const renderCommunityPage = (shopItemState: ShopItemState) => {
+const renderCommunityPage = (shopItemState: ShopItemState, reviewState: ReviewState) => {
   renderWithProviders(
     <MemoryRouter initialEntries={['/community']}>
       <Routes>
         <Route path="/community" element={<CommunityPage />} />
       </Routes>
-    </MemoryRouter>, { preloadedState: {shopitem: shopItemState}}
+    </MemoryRouter>, { preloadedState: {shopitem: shopItemState, review: reviewState}}
   )
 }
 
 describe('<CommunityPage />', () => {
   it('should render without error', () => {
-    renderCommunityPage(stubShopItemState)
+    renderCommunityPage(stubShopItemState, stubReviewState)
   })
 })
