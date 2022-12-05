53c833394bb02a3742a7d2dac71ce975339d44eb
diffzerg <dusxodud98@naver.com>
2022-12-05 17:12:56 +0000

    fixed scripts

--- backend/run_backend.sh
+++ backend/run_backend.sh
@@ -3,6 +3,7 @@
 # backend/run_backend.sh
 #!/bin/bash
 # TODO: Write automation script for launching BE app
+pip install -r requirements.txt
 python manage.py makemigrations 
 python manage.py migrate
 mkdir -p /log # for `uwsgi` logging uwsgi --ini uwsgi/uwsgi.ini
