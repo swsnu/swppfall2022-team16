66168e2d7dfcfd105bce92e612e6456445f3515e
Young-in <daddy321@snu.ac.kr>
2022-12-09 15:29:43 +0000

    Fix failed tests

--- frontend/src/components/OrderForm.test.tsx
+++ frontend/src/components/OrderForm.test.tsx
@@ -1,24 +1,46 @@
-import { fireEvent, render, screen, waitFor} from "@testing-library/react";
-import OrderForm from "./OrderForm";
+import React from 'react'
+import { fireEvent, render, screen, waitFor} from "@testing-library/react"
+import OrderForm from "./OrderForm"
+import { renderWithProviders, stubShopItemState, stubUserOrderState, stubUserState } from '../test-utils/mock'
+import { ShopItemState } from '../store/slices/shopitem'
+import { UserState } from '../store/slices/user'
+import { UserOrderState } from '../store/slices/userorder'
 
-describe("<Filter />", () => {
-    it("should render without errors", async () => {
-        render(<OrderForm
-            imageURL = "testURL"
-            itemName = "BridgeUs"
-            sellerName = "team16"
-            color = "Black"
-            size = "L"
-            quantity = {1}/>
-            );
-            screen.getByAltText("itemimage");
-            screen.getByText("BridgeUs");
-            screen.getByText("team16");
-            screen.getByText("Color");
-            screen.getByText("Size");
-            screen.getByText("Quantity");
-            screen.getByText("Black");
-            screen.getByText("L");
-            screen.getByText("1");
-        })
+const mockNavigate = jest.fn()
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useNavigate: () => mockNavigate
+}))
+
+const mockDispatch = jest.fn()
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch
+}))
+
+const renderOrderForm = (shopItemState: ShopItemState, userState: UserState, userOrderState: UserOrderState): any => {
+  renderWithProviders(
+    <OrderForm
+      orderID={1}
+      itemID={1}
+      color={'Black'}
+      size={'L'}
+      quantity={1}
+      price={100} />,
+    { preloadedState: { shopitem: shopItemState, user: userState, userorder: userOrderState } }
+  )
+}
+
+describe("<OrderForm />", () => {
+  it("should render without errors", async () => {
+    renderOrderForm(stubShopItemState, stubUserState, stubUserOrderState)
+    await screen.findByAltText("itemimage")
+    screen.getByAltText("itemimage")
+    screen.getByText("Color")
+    screen.getByText("Size")
+    screen.getByText("Quantity")
+    screen.getByText("Black")
+    screen.getByText("L")
+    screen.getByText("1")
+  })
 });
